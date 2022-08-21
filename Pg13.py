af79fc92729aced76b3c4fac15d8af5a331c0102
Gaurav Gali <gaurav.gali2k@gmail.com>
2022-08-21 21:06:53 +0530

    Pg13 Commit

---
+++ Pg13.py
@@ -0,0 +1,10 @@
+# Update Dictionary with user given values
+
+d = {} # empty dictioanry for referencing later
+
+k = input('Enter the key : ')
+v = eval(input(('Enter the value : ')))
+
+d[k] = v # updating
+
+print('The Updated Dictionary : ' , d)
